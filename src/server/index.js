dd7cbd073043d8b414ffc13f42934198cedb00c2
Alex H <alex.holder@bigfootds.com>
2023-07-06 16:43:30 +1000

    feat: server config init

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,22 @@
+// Make any environment variables ready for use if they're in a .env file
+const dotenv = require('dotenv');
+dotenv.config();
+
+// If no process.env.X is found, assign a default value instead.
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 7474;
+/* 
+Wanna be inline with Servarr ports
+Radarr = 7878
+Sonarr = 8989
+Lidarr = 8686
+Readarr = 8787
+Whisparr = 6969
+Prowlarr = 9696
+Sourcepool = 7474
+D&D1E was released in 1974.
+*/
+
 const {app} = require('./server.js');
 
 app.listen(3000, () => {
--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 
-var cors = require('cors')
+// Configure some basic Helmet settings on the server instance.
+const helmet = require('helmet');
+app.use(helmet());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
+app.use(helmet.contentSecurityPolicy({
+    directives:{
+        defaultSrc:["'self'"]
+    }
+}));
 
+// CORS settings
+// For now, just allow all traffic from all sources to reach this API.
+var cors = require('cors')
 app.use(cors());
 
 app.get("/", (request, response) => {
