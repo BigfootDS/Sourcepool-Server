589c58861505a58139c63cbdb9581085fad892eb
Alex H <alex.holder@bigfootds.com>
2023-08-29 14:08:59 +1000

    docs: model JSDoc refactoring in progress

--- src/server/models/CharacterModel.js
+++ src/server/models/CharacterModel.js
@@ -7,10 +7,27 @@ const { Source } = require('./SourceModel');
 const { User } = require('./UserModel');
 
 
+
+/**
+ * Represents both player-characters and non-player characters, including creatures/monsters. 
+ * Everyone has a stat block!
+ * @date 8/29/2023 - 11:25:34 AM
+ * @author BigfootDS
+ *
+ * @property {Boolean} isPlayable Required, default false. Flag should be true if players can use this character and modify its data.
+ * @property {Player} player Optional. Cannot have a value if this.isPlayable is false. This represents the non-admin and non-manager user who is allowed to use this character and modify its data.
+ * @property {Number} healthCurrent Required, default 10. Hit points (HP) that this character currently has. 0 = unconscious or dead.
+ * @property {Number} healthMax Required, default 10. The maximum amount of hit points (HP) that this character can have. Should never be lower than this.healthCurrent.
+ * @property {Number} healthBonus Required, default 0. Additional hit points that are consumed at a higher priority than regular hit points. Usually 0, increases and resets to 0 in specific conditions or events.
+ * @property {Number} deathSaveFails Optional. Not all characters will ever need to track this - it's usually just player characters or critical non-player characters. Should be 0 if this.healthCurrent is greater than 0.
+ * @property {Number} deathSaveSuccesses Optional. Not all characters will ever need to track this - it's usually just player characters or critical non-player characters. Should be 0 if this.healthCurrent is greater than 0.
+ * 
+ * @class
+ * @extends {Source}
+ */
 class Character extends Source{
 	constructor(){
 		super();
-
 
 		this.isPlayable = {
 			type: Boolean,
@@ -18,6 +35,11 @@ class Character extends Source{
 			default: false
 		};
 
+		this.player = {
+			type: User,
+			required: false
+		}
+		
 		this.healthCurrent = {
 			type: Number,
 			required: true,
@@ -36,10 +58,16 @@ class Character extends Source{
 			default: 0
 		}
 
-		this.player = {
-			type: User,
+		this.deathSaveFails = {
+			type: Number,
 			required: false
 		}
+
+		this.deathSaveSuccesses = {
+			type: Number,
+			required: false
+		}
+
 
 		this.abilityScores = {
 			type: [AbilityScore],
@@ -82,11 +110,6 @@ class Character extends Source{
 			type: [DamageMultiplier],
 			required: false
 		}
-
-		this.tags = {
-			type: [String],
-			required: true
-		}
 	}
 
 	static collectionName() {
@@ -96,7 +119,6 @@ class Character extends Source{
 
 }
 
-
 module.exports = {
-	Character: Character
+	Character
 }
--- src/server/models/CharacterSkillModel.js
+++ src/server/models/CharacterSkillModel.js
@@ -1,15 +1,40 @@
+const { Ability } = require('./AbilityModel');
 const { Skill } = require('./SkillModel');
 
 const EmbeddedDocument = require('camo').EmbeddedDocument;
 
+/**
+ * Basically a join table between Skill and a character.
+ * As an embedded document, this only exists within other documents, such as characters.
+ * @date 8/29/2023 - 11:10:01 AM
+ * @author BigfootDS
+ *
+ * @class
+ * @property {Boolean} applyProficiencyBonus Required, default false. Flag to determine if a character's proficiency bonus should be applied to a skill check for this skill.
+ * @property {Ability} abilityOverride Optional. Allows a character to perform a skill check using a different base ability, such as making an Intimidation check via the Strength ability instead of the Charisma ability.
+ * @property {Number} proficiencyBonusMultiplier Required, default 1. Applies to the proficiency bonus added to a skill check for this skill.
+ * @property {SKill} skill Required. The specific skill affected by the multiplier.
+ * @extends {EmbeddedDocument}
+ */
 class CharacterSkill extends EmbeddedDocument{
 	constructor(){
 		super();
 
-		this.multiplier = {
+		this.applyProficiencyBonus = {
+			type: Boolean,
+			required: true,
+			default: false,
+		}
+
+		this.abilityOverride = {
+			type: Ability,
+			required: false
+		}
+
+		this.proficiencyBonusMultiplier = {
 			type: Number,
 			required: true,
-			default: 1,
+			default: 1
 		}
 
 		this.skill = {
