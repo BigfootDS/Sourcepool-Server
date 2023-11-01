f3c956351e73aa43bee92b790aeb66f9175206c0
Alex H <alex.holder@bigfootds.com>
2023-11-02 09:02:11 +1100

    docs: add comments to models

--- src/server/models/EncounterModel.js
+++ src/server/models/EncounterModel.js
@@ -3,6 +3,14 @@ const { Place } = require('./PlaceModel');
 const { Source } = require('./SourceModel');
 
 
+/**
+ * Model to track progression through an Encounter, whether it's combat or social does not matter.
+ * This is really just for a human (eg. a DM/GM) to see which character currently needs to do things.
+ * @date 11/2/2023 - 9:01:31 AM
+ * @author BigfootDS
+ *
+ * @extends {Source}
+ */
 class Encounter extends Source{
 	constructor(){
 		super();
--- src/server/models/InitiativeEmbeddedModel.js
+++ src/server/models/InitiativeEmbeddedModel.js
@@ -2,6 +2,14 @@ const { Character } = require('./CharacterModel');
 
 const EmbeddedDocument = require('camo').EmbeddedDocument;
 
+
+/**
+ * Model to track which character has what positioning in an Encounter.
+ * @date 11/2/2023 - 9:01:09 AM
+ * @author BigfootDS
+ *
+ * @extends {EmbeddedDocument}
+ */
 class Initiative extends EmbeddedDocument {
 	constructor(){
 		super();
