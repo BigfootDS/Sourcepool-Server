4e7f0ec6029dbf4280c5fd7f423af730973e8ee2
Alex H <alex.holder@bigfootds.com>
2023-11-04 18:12:41 +1100

    feat: Ability CRUD routes implemented, no authorisation though!

--- src/server/controllers/AbilitiesController.js
+++ src/server/controllers/AbilitiesController.js
@@ -4,34 +4,111 @@ const router = require('express').Router();
 
 // Get all objects of this model.
 router.get("/all", async (request, response) => {
-	let result = await Ability.find({});
+	let result = await Ability.find({}).catch(error => error);
 
 	response.json({
 		result
 	});
 });
 
-// Get one object.
+// Get one or many objects.
 // If no key is provided, or if a key is provided without a value, this returns nothing.
-router.get("/one/:key/:value", async (request, response) => {
+router.get("/multiple/:key/:value", async (request, response) => {
+	// Our data structure is too nested to do this simply:
+	//let result = await Ability.find({[request.params.key]: request.params.value}).catch(error => error);
+	let results = [];
 
+	// let allDocuments = await Ability.find({});
+
+	switch (request.params.key) {
+		case "name":
+			results = await Ability.find({"description.name": {$regex: new RegExp(`(${request.params.value})`, 'gi')}}).catch(error => error);
+			break;
+		case "tags":
+		case "tag":
+			results = await Ability.find({"tags": request.params.value}).catch(error => error);
+			break;
+		default:
+			break;
+	}
+
+	response.json({
+		results
+	})
 });
 
 // Create new object.
 // Expects object data to be on the request body.
+// Expects request.body to look like:
+/*
+{
+    "tags":"testo",
+    "description": [
+        {
+            "language":"en",
+            "name":"Testo Ability",
+            "content":"Test of functionality."
+        },
+                {
+            "language":"fr",
+            "name":"Ability de Testo",
+            "content":"Test de logicel."
+        }
+    ]
+}
+*/
 router.post("/one", async (request, response) => {
+	// let newAbility = await Ability.create({
+	// 	tags: request.body.tags,
+	// 	description: [
+	// 		await Lore.create({
+	// 			language: "en",
+	// 			name:ability.name,
+	// 			content:ability.content
+	// 		})
+	// 	],
+	// }).save();
 
+	let newAbility = await Ability.create(request.body).save().catch(error => error);
+
+	response.json({
+		result: newAbility
+	});
 });
 
 // Edit whole object and return whole object.
 // Expects object data to be on the request body.
+// Expects request.body to look like:
+/*
+{
+	targetDocId: "askhgdbfa",
+	newData: {
+		someProperty: "someValue"
+	}
+}
+*/
 router.put("/one", async (request, response) => {
+	let result = await Ability.findOne({_id: request.body.targetDocId});
+	console.log(result);
+	Object.keys(request.body.newData).forEach((key, index) => {
+		console.log(key, result[key]);
+		result[key] = request.body.newData[key]
+	});
 
+	let savedResult = await result.save();
+
+	response.json({
+		result: savedResult
+	});
 });
 
 // Delete an object and return a success/fail result of the delet operation.
 router.delete("/one", async (request, response) => {
+	let result = await Ability.findOneAndDelete({_id: request.body.targetDocId});
 
+	response.json({
+		result: result
+	});
 });
 
 module.exports = router;
